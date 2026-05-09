a46a2aa11e3cc6a20ea59d48ad2b62cfe615323f
gaboparra <geparra06@gmail.com>
2026-05-09 04:39:26 -0300

    toggle instructions

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -370,5 +370,25 @@ function showFinalData(songData) {
   setPlayIcon(false);
 }
 
+function toggleInstructions() {
+  const modal = document.getElementById("instructions-modal");
+  modal.classList.toggle("show");
+}
+
+// cerrar si clickean afuera
+document.addEventListener("click", function(e){
+  const modal = document.getElementById("instructions-modal");
+  const button = document.querySelector(".help-btn");
+
+  if (!modal || !button) return;
+
+  if (
+    !modal.contains(e.target) &&
+    !button.contains(e.target)
+  ){
+    modal.classList.remove("show");
+  }
+});
+
 // ── INIT ─────────────────────────────────────────────────────────────────────
 checkSession();
